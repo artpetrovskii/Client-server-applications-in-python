fbe3ab848143cdfdbd941fb10999c873999c1839
artpetrovskii <46283509+artpetrovskii@users.noreply.github.com>
2020-07-19 19:16:16 +0300

    Update main_json.py

--- main_json.py
+++ main_json.py
@@ -11,7 +11,6 @@ def write_order_to_json(item, quantity, price, buyer, date):
                       'price': price, 'buyer': buyer, 'date': date}
         orders_list.append(order_info)
         json.dump(data, f_in, indent=4)
-
-write_order_to_json('printer', '10', '6700', 'Ivanov I.I.', '24.09.2017')
-write_order_to_json('scaner', '20', '10000', 'Petrov P.P.', '11.01.2018')
-write_order_to_json('computer', '5', '40000', 'Sidorov S.S.', '2.05.2019')
+    write_order_to_json('printer', '10', '6700', 'Ivanov I.I.', '24.09.2017')
+    write_order_to_json('scaner', '20', '10000', 'Petrov P.P.', '11.01.2018')
+    write_order_to_json('computer', '5', '40000', 'Sidorov S.S.', '2.05.2019')
