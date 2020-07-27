8a6de7526b05b27c058b9cf3cd3330f19bd0b910
Artemii <artemii.petrovskii@outlook.com>
2020-07-27 18:00:16 +0300

    Lesson5

--- client.py
+++ client.py
@@ -1,14 +1,19 @@
+from errors import RequiredFieldMissingError
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+                            RESPONSE, DEFAULT_PORT, ERROR, DEFAULT_IP_ADDRESS
+from common.utils import get_message, send_message
 import sys
 import json
 import socket
 import time
-from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
-                             RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
-from common.utils import get_message, send_message
+import argparse
+import logging
+import logs.config_client_log
 
+
+logging_client = logging.getLogger('client')
 
 def create_presence(account_name='Guest'):
-    # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
     out = {
         ACTION: PRESENCE,
         TIME: time.time(),
@@ -16,41 +21,56 @@ def create_presence(account_name='Guest'):
             ACCOUNT_NAME: account_name
         }
     }
+    logging_client.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
     return out
 
 
-def process_ans(message):
+def process_answer(message):
+    logging_client.debug(f'Разбор сообщения от сервера: {message}')
     if RESPONSE in message:
         if message[RESPONSE] == 200:
             return '200 : OK'
         return f'400 : {message[ERROR]}'
-    raise ValueError
+    raise RequiredFieldMissingError(RESPONSE)
+
+
+def create_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
+    parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
+    return parser
 
 
 def main():
-    # client.py 95.161.223.252 34828
-    try:
-        server_address = sys.argv[1]
-        server_port = int(sys.argv[2])
-        if server_port < 1024 or server_port > 65535:
-            raise ValueError
-    except IndexError:
-        server_address = DEFAULT_IP_ADDRESS
-        server_port = DEFAULT_PORT
-    except ValueError:
-        print('В качестве порта может быть указано только '
-              'число в диапазоне от 1024 до 65535.')
+    parser = create_argument_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    server_address = namespace.addr
+    server_port = namespace.port
+
+    if not 1023 < server_port < 65536:
+        logging_client.critical(
+            f'Попытка запуска клиента с неподходящим номером порта: {server_port}.'
+            f' Допустимы адреса с 1024 до 65535. Клиент завершается.')
         sys.exit(1)
 
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.connect((server_address, server_port))
-    message_to_server = create_presence()
-    send_message(transport, message_to_server)
+    logging_client.info(f'Запущен клиент с парамертами: '
+                       f'адрес сервера: {server_address}, порт: {server_port}')
     try:
-        answer = process_ans(get_message(transport))
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.connect((server_address, server_port))
+        message_to_server = create_presence()
+        send_message(transport, message_to_server)
+        answer = process_answer(get_message(transport))
+        logging_client.info(f'Принят ответ от сервера {answer}')
         print(answer)
-    except (ValueError, json.JSONDecodeError):
-        print('Не удалось декодировать сообщение сервера.')
+    except json.JSONDecodeError:
+        logging_client.error('Не удалось декодировать полученную Json строку.')
+    except RequiredFieldMissingError as missing_error:
+        logging_client.error(f'В ответе сервера отсутствует необходимое поле '
+                             f'{missing_error.missing_field}')
+    except ConnectionRefusedError:
+        logging_client.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+                                f'конечный компьютер отверг запрос на подключение.')
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,14 +1,21 @@
+from errors import IncorrectDataReceivedError
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+                             RESPONSE, DEFAULT_PORT, MAX_CONNECTIONS, ERROR
+from common.utils import get_message, send_message
 import socket
 import sys
+import argparse
 import json
-from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
-                             PRESENCE, TIME, USER, ERROR, DEFAULT_PORT
-from common.utils import get_message, send_message
+import logging
+import logs.config_server_log
 
+
+SERVER_LOGGER = logging.getLogger('server')
 
 def process_client_message(message):
-    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
-            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+    SERVER_LOGGER.debug(f'Разбор сообщения от клиента : {message}')
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message and \
+            USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
         return {RESPONSE: 200}
     return {
         RESPONSE: 400,
@@ -16,31 +23,26 @@ def process_client_message(message):
     }
 
 
-def main():
-    try:
-        if '-p' in sys.argv:
-            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
-        else:
-            listen_port = DEFAULT_PORT
-        if listen_port < 1024 or listen_port > 65535:
-            raise ValueError
-    except IndexError:
-        print('После параметра -\'p\' необходимо указать номер порта.')
-        sys.exit(1)
-    except ValueError:
-        print('В качастве порта может быть указано только'
-              'число в диапазоне от 1024 до 65535.')
-        sys.exit(1)
-    try:
-        if '-a' in sys.argv:
-            listen_address = sys.argv[sys.argv.index('-a') + 1]
-        else:
-            listen_address = ''
+def create_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-a', default='', nargs='?')
+    return parser
 
-    except IndexError:
-        print('После параметра \'a\'- необходимо указать адрес,'
-              'который будет слушать сервер.')
+
+def main():
+    parser = create_arg_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_address = namespace.a
+    listen_port = namespace.p
+
+    if not 1023 < listen_port < 65536:
+        SERVER_LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта '
+                               f'{listen_port}. Допустимы адреса с 1024 до 65535')
         sys.exit(1)
+    SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
+                       f'адрес с которого принимаются подключения: {listen_address} '
+                       f'Если адрес не указан, принимаются соединения с любых адресов')
 
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transport.bind((listen_address, listen_port))
@@ -48,15 +50,22 @@ def main():
 
     while True:
         client, client_address = transport.accept()
+        SERVER_LOGGER.info(f'Установлено соедение с ПК {client_address}')
         try:
-            message_from_cient = get_message(client)
-            print(message_from_cient)
-            # {'action': 'presence', 'time': 1573760672.167031, 'user': {'account_name': 'Guest'}}
-            response = process_client_message(message_from_cient)
+            message_from_client = get_message(client)
+            SERVER_LOGGER.debug(f'Получено сообщение {message_from_client}')
+            response = process_client_message(message_from_client)
+            SERVER_LOGGER.info(f'Cформирован ответ клиенту {response}')
             send_message(client, response)
+            SERVER_LOGGER.debug(f'Соединение с клиентом {client_address} закрывается')
             client.close()
-        except (ValueError, json.JSONDecodeError):
-            print('Принято некорретное сообщение от клиента.')
+        except json.JSONDecodeError:
+            SERVER_LOGGER.error(f'Не удалось декодировать JSON строку, полученную от '
+                                f'клиента {client_address}. Соединение закрывается')
+            client.close()
+        except IncorrectDataReceivedError:
+            SERVER_LOGGER.error(f'От клиента {client_address} приняты некорректные данные '
+                                f'Соединение закрывается')
             client.close()
 
 
