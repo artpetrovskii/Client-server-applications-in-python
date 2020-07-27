d79b151c110a26d9545edafc372bfce60a9e9914
Artemii <artemii.petrovskii@outlook.com>
2020-07-27 18:02:16 +0300

    test

--- task_6.py
+++
@@ -1,7 +0,0 @@
-with open('test_file.txt', 'w', encoding='cp866') as t_f:
-    t_f.write('сетевое программирование\n')
-    t_f.write('сокет\n')
-
-with open('test_file.txt', encoding='cp866') as t_f:
-    for each_line in t_f:
-        print(each_line, end="")
