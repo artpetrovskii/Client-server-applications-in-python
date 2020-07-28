378733c7b24522a0a66b018bd0ef5308c0243d69
artpetrovskii <46283509+artpetrovskii@users.noreply.github.com>
2020-07-28 14:33:13 +0300

    Update utils.py

--- Lesson5/common/utils.py
+++ Lesson5/common/utils.py
@@ -1,16 +1,7 @@
-"""Утилиты"""
-
 import json
 from common.variables import MAX_PACKAGE_LENGTH, ENCODING
 
 def get_message(client):
-    '''
-    Утилита приёма и декодирования сообщения
-    принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку значения
-    :param client:
-    :return:
-    '''
-
     encoded_response = client.recv(MAX_PACKAGE_LENGTH)
     if isinstance(encoded_response, bytes):
         json_response = encoded_response.decode(ENCODING)
@@ -22,14 +13,6 @@ def get_message(client):
 
 
 def send_message(sock, message):
-    '''
-    Утилита кодирования и отправки сообщения
-    принимает словарь и отправляет его
-    :param sock:
-    :param message:
-    :return:
-    '''
-
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
     sock.send(encoded_message)
