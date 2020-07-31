b6bdd5b7c663c823de203441a06bc019012cd2e3
Artemii <artemii.petrovskii@outlook.com>
2020-07-31 14:00:56 +0300

    Lesson6

---
+++ Lesson6/errors.py
@@ -0,0 +1,14 @@
+class IncorrectDataReceived(Exception):
+    def __str__(self):
+        return 'Принято некорректное сообщение от удалённого компьютера'
+
+class NoDictInput(Exception):
+    def __str__(self):
+        return 'Аргумент функции должен быть словарём'
+
+class MissingRequiredField(Exception):
+    def __init__(self, missing_field):
+        self.missing_field = missing_field
+
+    def __str__(self):
+        return f'В принятом словаре отсутствует обязательное поле {self.missing_field}'
