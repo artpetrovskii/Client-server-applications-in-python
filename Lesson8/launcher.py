adf10d5ca13d178c5c06fa09df443ed347c29bdf
artpetrovskii <46283509+artpetrovskii@users.noreply.github.com>
2020-08-08 15:33:23 +0300

    Update launcher.py

--- Lesson8/launcher.py
+++ Lesson8/launcher.py
@@ -10,14 +10,18 @@ while True:
     if ACTION == 'q':
         break
     elif ACTION == 's':
-        PROCESSES.append(subprocess.Popen('python server.py',
-                                          creationflags=subprocess.CREATE_NEW_CONSOLE))
-        PROCESSES.append(subprocess.Popen('python client.py -n test1',
-                                          creationflags=subprocess.CREATE_NEW_CONSOLE))
-        PROCESSES.append(subprocess.Popen('python client.py -n test2',
-                                          creationflags=subprocess.CREATE_NEW_CONSOLE))
-        PROCESSES.append(subprocess.Popen('python client.py -n test3',
-                                          creationflags=subprocess.CREATE_NEW_CONSOLE))
+        PROCESSES.append(subprocess.Popen(
+            'python server.py',
+            creationflags=subprocess.CREATE_NEW_CONSOLE))
+        PROCESSES.append(subprocess.Popen(
+            'python client.py -n test1',
+            creationflags=subprocess.CREATE_NEW_CONSOLE))
+        PROCESSES.append(subprocess.Popen(
+            'python client.py -n test2',
+            creationflags=subprocess.CREATE_NEW_CONSOLE))
+        PROCESSES.append(subprocess.Popen(
+            'python client.py -n test3',
+            creationflags=subprocess.CREATE_NEW_CONSOLE))
     elif ACTION == 'x':
         while PROCESSES:
             VICTIM = PROCESSES.pop()
