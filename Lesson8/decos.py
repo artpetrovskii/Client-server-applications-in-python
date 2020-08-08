64cda0c6c947299ec73c729515a10bc1c5fae319
artpetrovskii <46283509+artpetrovskii@users.noreply.github.com>
2020-08-08 15:38:40 +0300

    Update decos.py

--- Lesson8/decos.py
+++ Lesson8/decos.py
@@ -13,7 +13,8 @@ else:
 def log(func_to_log):
     def log_saver(*args, **kwargs):
         ret = func_to_log(*args, **kwargs)
-        LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}. '
+        LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c '
+                     'параметрами {args}, {kwargs}. '
                      f'Вызов из модуля {func_to_log.__module__}')
         return ret
     return log_saver
