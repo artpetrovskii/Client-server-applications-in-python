8f958eae3b609b3752bb6c10b0263615c8307a42
artpetrovskii <46283509+artpetrovskii@users.noreply.github.com>
2020-08-08 15:36:38 +0300

    Update config_server_log.py

--- Lesson8/logs/config_server_log.py
+++ Lesson8/logs/config_server_log.py
@@ -6,7 +6,8 @@ from common.variables import LOGGING_LEVEL
 sys.path.append('../')
 
 
-SERVER_FORMATTER = logging.Formatter('%(asctime)s %(levelname)s %(filename)s %(message)s')
+SERVER_FORMATTER = logging.Formatter('%(asctime)s %(levelname)s '
+                                     '%(filename)s %(message)s')
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 PATH = os.path.join(PATH, 'server.log')
@@ -14,7 +15,8 @@ PATH = os.path.join(PATH, 'server.log')
 STREAM_HANDLER = logging.StreamHandler(sys.stderr)
 STREAM_HANDLER.setFormatter(SERVER_FORMATTER)
 STREAM_HANDLER.setLevel(logging.ERROR)
-LOG_FILE = logging.handlers.TimedRotatingFileHandler(PATH, encoding='utf8', interval=1, when='D')
+LOG_FILE = logging.handlers.TimedRotatingFileHandler(PATH, encoding='utf8',
+                                                     interval=1, when='D')
 LOG_FILE.setFormatter(SERVER_FORMATTER)
 
 LOGGER = logging.getLogger('server')
